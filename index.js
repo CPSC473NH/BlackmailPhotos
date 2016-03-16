805dc8a493347517dc2d680ab8e18be99d641369
Brendon Hollingsworth <tucaeg@yahoo.com>
2016-03-15 20:18:58 -0700

    added signin and sign up modals. Got it to post

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+
+
 $(document).ready(function() {
   "use strict";
+
+
+
+  $("#signup").click(function() {
+    $("#signinButton").hide();
+    $("#registerButton").show();
+    $("#registerSignin-title").empty().append("Sign up");
+    $("#registerSignIn").modal();
+  });
+
+  $("#registerButton").click(function() {
+    var registrationData = _.object($("#registerSignIn-form").serializeArray().map(function(v) {return [v.name, v.value];} ));  //returns form values as key value pairs
+    $.post("http://localhost:3000/accounts", {
+      "username": registrationData.email,
+      "password": registrationData.password
+    }, function() {
+      //put an alert in here for now
+      alert("successfully posted!");
+    });
+  });
+
+  $("#signin").click(function() {
+    $("#registerButton").hide();
+    $("#signinButton").show();
+    $("#registerSignin-title").empty().append("Sign in");
+    $("#registerSignIn").modal();
+  });
+  $("#signinButton").click(function() {
+    var signinData = _.object($("#registerSignIn-form").serializeArray().map(function(v) {return [v.name, v.value];} ));//converts form data to associative array
+      //TODO: post for signing in
+  });
 });
