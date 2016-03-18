d0f0757f8cf933de2e7a0556d2b273dca3435a05
Skyler Lister Aley <skylerlisteraley@gmail.com>
2016-03-18 01:44:45 -0700

    Made some changes to "create blackmail modal" and "view your blackmail"
    Made some changes to "create blackmail modal" and "view your blackmail".
    Added all dependency to the root folder so a sync should give everyone
    the needed files.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-
-
+//GLYPHICONS Halflings font is also released as an extension of a Bootstrap www.getbootstrap.com for free and it is released under the same license as Bootstrap. 
+//Thank you to GLYPHICONS.com for GLYPHICONS Halflings
 $(document).ready(function() {
   "use strict";
 
@@ -12,11 +12,25 @@ $(document).ready(function() {
   var $errorMsg = document.getElementById("errorMsg");
   var $currentUser = ""; 
 
+  // initialize input widgets first
+  $("#createModal .time").timepicker({
+      'timeFormat': 'h:i a',
+      'disableTextInput': true,
+  });
+
+  $("#createModal .date").datepicker({
+      'format': "mm/dd/yyyy",
+      'autoclose': true,
+      'todayHighlight': true,
+      'todayBtn': "linked",
+      'startDate': "today",
+  });
+
   $("#signup").click(function() {
-    $("#signinButton").hide();
-    $("#registerButton").show();
-    $("#registerSignin-title").empty().append("Sign up");
-    $("#registerSignIn").modal();
+  $("#signinButton").hide();
+  $("#registerButton").show();
+  $("#registerSignin-title").empty().append("Sign up");
+  $("#registerSignIn").modal();
   });
 
   $("#registerButton").click(function() {
@@ -61,7 +75,9 @@ $(document).ready(function() {
           "recName": createData.recName,
           "recEmail": createData.recEmail,
           "date": createData.date,
+          "time": createData.time,
           "demands": createData.demands,
+          "demandsMet": false,
         }, function() {
           $("#createModal").modal("hide");
     });
@@ -69,20 +85,27 @@ $(document).ready(function() {
 
   $("#viewBlackmail").click(function() {
       $.get("http://localhost:3000/blackmails", {"creator": $currentUser}, function(data) {
-        $("#mainHeader").empty().append("Your Blackmails");
+        $("#mainHeader").empty().append('<span class="glyphicon glyphicon-envelope " aria-hidden="true"></span> Your Blackmails');
         $("#blackmailDisp").empty();
         $("#viewList").empty();
         for(var i = 0; i < data.length; i++)
         {
+          var deadline = 'December 31 2016';
           var $singleMail = '<div class="col-lg-3 col-md-4 col-xs-6 thumb">';
           $singleMail += '<a class="thumbnail" href="#">';
-          $singleMail += '<img class="img-responsive" src="http://placehold.it/400x300" alt="'+data[0].title+'"></a>';
+          $singleMail += '<img class="img-responsive" src="http://placehold.it/400x300" alt="'+data[i].title+'"></a>';
           $singleMail += '<ul class="list-group">';
-          $singleMail += '<li class="list-group-item">Title: '+data[0].title;
-          $singleMail += '<li class="list-group-item">Recipient Name: '+data[0].recName;
-          $singleMail += '<li class="list-group-item">Recipient Email: '+data[0].recEmail;
-          $singleMail += '<li class="list-group-item">Demands: '+data[0].demands;
-          $singleMail += '<li class="list-group-item">Release Date: '+data[0].date;
+          $singleMail += '<li class="list-group-item"><b>Title</b>: '+data[i].title;
+          $singleMail += '<li class="list-group-item"><b>Recipient Name</b>: '+data[i].recName;
+          $singleMail += '<li class="list-group-item"><b>Recipient Email</b>: '+data[i].recEmail;
+          $singleMail += '<li class="list-group-item"><b>Demands</b>: '+data[i].demands;
+          var $icon = '<span class="glyphicon glyphicon-remove " style="color:red" aria-hidden="true"></span>';
+          if(data[i].demandsMet){
+            $icon = '<span class="glyphicon glyphicon-ok green" style="color:green" aria-hidden="true"></span>';
+          }
+          $singleMail += '<li class="list-group-item"><b>Demands Met? </b>: '+$icon;
+          $singleMail += '<li class="list-group-item"><b>Release Date</b>: '+data[i].date;
+          $singleMail += '<li class="list-group-item"><b>Release Time</b>: '+data[i].time;
           $("#blackmailDisp").append($singleMail);
         }
       });
@@ -105,7 +128,7 @@ $(document).ready(function() {
           $viewBlackmail.style.display = "list-item";
           $logOut.style.display = "list-item";
 
-          var $greeting = '<span class="bg-primary" id="greeting">Hello, ' + data[0].username + '!</li>';
+          var $greeting = '<span class="text-primary" id="greeting">Hello, ' + data[0].username + '!</li>';
           $currentUser = data[0].username;
           $("#navbar").append($greeting);
         }
@@ -119,8 +142,27 @@ $(document).ready(function() {
     $createBlackmail.style.display = "none";
     $viewBlackmail.style.display = "none";
     $logOut.style.display = "none";
-    $("#mainHeader").empty().append("Blackmail Gallery");
-    $("#blackmailDisp").empty();
     $("#greeting").remove();
+    makeGallery();
   });
+
+  $("#brand").click(function() {
+    makeGallery();
+  });
+
+  $("#home").click(function() {
+    makeGallery();
+  });
+
+  function makeGallery(){
+    $("#mainHeader").empty().append('<span class="glyphicon glyphicon-envelope " aria-hidden="true"></span> The Blackmail Gallery');
+    $("#blackmailDisp").empty();
+    var $singleMail ='<div class="col-lg-3 col-md-4 col-xs-6 thumb">'
+    $singleMail += '<a class="thumbnail" href="#">';
+    $singleMail += '<img class="img-responsive" src="http://placehold.it/400x300" alt="">';
+    for(var i = 0; i < 12; i++)
+    {
+        $("#blackmailDisp").append($singleMail);
+    }
+  }
 });
