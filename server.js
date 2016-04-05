ba1d99a82ea6aae355c554ebcb2e978c1126d71b
hururuzz <seonghyeon.lim@outlook.com>
2016-04-04 21:32:52 -0700

    SMTP added

--- server.js
+++ server.js
@@ -35,4 +35,32 @@ app.post('/upload', upload.single('image'), function(req,res){
     "randomCode": req.body.randomCode
   });
   res.status(200).end()
+  
+    var creator = req.body.creator;
+  var date = req.body.date;
+  var demands = req.body.demands;
+  var demandMet = req.body.demandsMet;
+  var id = req.body.id;
+  var randomCode = req.body.randomCode;
+  var recEmail = req.body.recEmail;
+  var recName = req.body.recName;
+  var time = req.body.time;
+  var title = req.body.title;
+  var image = req.body.image;
+
+  //for SMTP feature
+  var emailTransporter = emailer.createTransport('smtps://blackmailappcpsc473%40@gmail.com:cpsc12345!@smtp.gmail.com');  
+  var mailOptions = {
+    from: 'Blackmailapp Admin <blackmailappcpsc473@gmail.com>',
+    to: recEmail,
+    subject: 'Blackmail Arrived!',
+    html: '<h1>'+ title + '</h1>' + '<h3>From: ' + recName + '<br>' + '<h3>Release Date: ' + date + ' ' + time + '<h3>Demands: ' + demands + '<h3>Image' + image
+  };
+  
+  emailTransporter.sendMail(mailOptions, function(error, info){
+     if(error){
+         return console.log(error);
+     } 
+     console.log('Message Sent!');
+  });
 });
